65ff8238322930c4f5e3abbdfa2c8b0e5a8063ce
Tannu kumari <tk5163324@gmail.com>
2026-06-20 23:46:05 +0530

    Fix allowed hosts for Render

--- Project_01/config/settings.py
+++ Project_01/config/settings.py
@@ -25,10 +25,7 @@ SECRET_KEY = 'django-insecure-j#j90%q(2pk=bsc!*ylidl9-78zudh&)ax4n)r-cu3(j@99%*v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost",
-]
+ALLOWED_HOSTS = ['my-quiz-genius.onrender.com', '127.0.0.1', 'localhost']
 
 # Application definition
 
