22e435a0edd77859cb61b017813b392b29581ac8
Tannu kumari <tk5163324@gmail.com>
2026-06-21 12:45:32 +0530

    Fix gitignore and remove media files

--- Project_01/accounts/views.py
+++ Project_01/accounts/views.py
@@ -1,176 +1,4 @@
-# from django.shortcuts import render, redirect
-
-# from django.contrib.auth.models import User
-
-# from django.contrib.auth import authenticate, login, logout
-
-# from django.contrib import messages
-
-
-
-
-# def register(request):
-
-
-#     if request.method == "POST":
-
-
-#         username = request.POST["username"]
-
-#         password = request.POST["password"]
-
-
-
-
-#         # Check username already exists
-
-#         if User.objects.filter(username=username).exists():
-
-
-#             messages.error(
-#                 request,
-#                 "Username already exists. Please choose another."
-#             )
-
-
-#             return redirect("register")
-
-
-
-
-
-#         # Create new user
-
-#         user = User.objects.create_user(
-
-#             username=username,
-
-#             password=password
-
-#         )
-
-
-#         user.save()
-
-
-
-#         messages.success(
-
-#             request,
-
-#             "Registration successful. Please login."
-
-#         )
-
-
-
-#         return redirect("login")
-
-
-
-
-
-#     return render(
-
-#         request,
-
-#         "register.html"
-
-#     )
-
-
-
-
-
-
-
-# def login_user(request):
-
-
-#     if request.method == "POST":
-
-
-
-#         username = request.POST["username"]
-
-#         password = request.POST["password"]
-
-
-
-
-#         user = authenticate(
-
-#             username=username,
-
-#             password=password
-
-#         )
-
-
-
-
-#         if user is not None:
-
-
-
-#             login(
-
-#                 request,
-
-#                 user
-
-#             )
-
-
-
-#             return redirect("/")
-
-
-
-
-#         else:
-
-
-#             messages.error(
-
-#                 request,
-
-#                 "Invalid username or password."
-
-#             )
-
-
-
-
-
-#     return render(
-
-#         request,
-
-#         "login.html"
-
-#     )
-
-
-
-
-
-
-
-# def logout_user(request):
-
-
-#     logout(request)
-
-
-#     return redirect("/")
-
-
-
-
-
-
-from django.shortcuts import render, redirect
+import django.shortcuts
 
 from django.contrib.auth.models import User
 
@@ -211,7 +39,7 @@ def register(request):
             )
 
 
-            return redirect("register")
+            return django.shortcuts.redirect("register")
 
 
 
@@ -244,22 +72,14 @@ def register(request):
         profile_pic = request.FILES.get("profile_pic")
 
 
+        Profile.objects.get_or_create(
+        user=user,
+        defaults={
+        "profile_pic": profile_pic
+    }
+)
 
-        Profile.objects.create(
-
-            user=user,
-
-            profile_pic=profile_pic
-
-        )
-
-
-
-
-
-        messages.success(
-
-            request,
+        messages.success(request,
 
             "Registration successful. Please login."
 
@@ -267,14 +87,14 @@ def register(request):
 
 
 
-        return redirect("login")
+        return django.shortcuts.redirect("login")
 
 
 
 
 
 
-    return render(
+    return django.shortcuts.render(
 
         request,
 
@@ -332,11 +152,7 @@ def login_user(request):
 
 
 
-            return redirect("/")
-
-
-
-
+            return django.shortcuts.redirect("/")
 
         else:
 
@@ -350,10 +166,7 @@ def login_user(request):
             )
 
 
-
-
-
-    return render(
+    return django.shortcuts.render(
 
         request,
 
@@ -363,15 +176,10 @@ def login_user(request):
 
 
 
-
-
-
-
-
 def logout_user(request):
 
 
     logout(request)
 
 
-    return redirect("/")
+    return django.shortcuts.redirect("/")
