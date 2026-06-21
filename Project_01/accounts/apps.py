ba66bed6a9c99e6b3c6e750d2a19c12a66069744
Tannu kumari <tk5163324@gmail.com>
2026-06-21 14:43:20 +0530

    Add profile picture feature

--- Project_01/accounts/apps.py
+++ Project_01/accounts/apps.py
@@ -3,3 +3,12 @@ from django.apps import AppConfig
 
 class AccountsConfig(AppConfig):
     name = 'accounts'
+
+
+
+class AccountsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'accounts'
+
+    def ready(self):
+        import accounts.signals
