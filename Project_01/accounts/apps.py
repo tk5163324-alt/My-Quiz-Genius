fdda01f485b7cd9a0ef584107a213f54cd90ebd2
Tannu kumari <tk5163324@gmail.com>
2026-06-21 14:52:28 +0530

    Fix profile picture model

--- Project_01/accounts/apps.py
+++ Project_01/accounts/apps.py
@@ -9,6 +9,3 @@ class AccountsConfig(AppConfig):
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
-
-    def ready(self):
-        import accounts.signals
--- Project_01/accounts/models.py
+++ Project_01/accounts/models.py
@@ -1,12 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_pic = models.ImageField(
-        upload_to='profile_pics/',
-        default='profile_pics/default.png'
-    )
-
-    def __str__(self):
-        return self.user.username
+# Create your models here.
